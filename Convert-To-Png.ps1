86652404c131d36e7dc7a4d5365b9c300401c804
Benjamin Abt <BenjaminAbt@users.noreply.github.com>
2016-08-31 21:04:04 +0200

    Create Convert-To-Png.ps1

---
+++ Convert-To-Png.ps1
@@ -0,0 +1,20 @@
+function ConvertTo-Png
+{
+    [cmdletbinding()]
+    param([Parameter(Mandatory=$true, ValueFromPipeline = $true)] $Path)
+
+    process{
+        if ($Path -is [string])
+        { $Path = get-childitem $Path }
+
+        $Path | foreach {
+            $image = [System.Drawing.Image]::FromFile($($_.FullName))
+            $FilePath =  "{0}\{1}.jpg" -f $($_.DirectoryName), $($_.BaseName)
+            $image.Save($FilePath, [System.Drawing.Imaging.ImageFormat]::Png)
+            $image.Dispose()
+        }
+    }
+
+ }
+
+ Get-ChildItem *.jpeg | ConvertTo-Png
